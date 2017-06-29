9e9c410853194837dadfc80da3ab2d4900937d82
Edgar <exgc12@gmail.com>
2017-06-29 15:51:37 -0400

    changed test db url

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                       'mongodb://localhost/blogPosts';
 exports.TEST_DATABASE_URL = (
   process.env.TEST_DATABASE_URL ||
-  'mongodb://localhost/test-blog-app'
+  'mongodb://localhost/blogPosts'
 );
 exports.PORT = process.env.PORT || 8080;
